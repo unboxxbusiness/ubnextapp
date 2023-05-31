5821a4418c51c2364f7c616d2d8d5d0f94611fc6
Unboxx Business - Start, Grow, and Scale Your Online Business <121428085+unboxxbusiness@users.noreply.github.com>
2023-05-31 16:57:18 +0530

    Add existing file

--- pages/success.js
+++ pages/success.js
@@ -21,7 +21,7 @@ const Success = () => {
     <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="text-left">
             <h2 className="text-3xl font-bold leading-tight text-black sm:text-3xl lg:text-4xl">Thank you for submitting your form! We have received your information and will be in touch with you shortly.</h2>
-            <Link href="https://youtube.com/@unboxxbusiness"><p className="mt-4 text-lg font-medium">Watch Latest Videos On Tech </p></Link>
+            <Link href="https://youtube.com/@unboxxbusiness"><p className="mt-4 text-lg font-medium">Watch Latest Videos On Tech🔥</p></Link>
            
       
         </div>
